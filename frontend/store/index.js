b91fb971ae9034fc3d8ed9a6a93ede8e409bbdf6
Ted Eckel <teckel1029@gmail.com>
2017-01-25 03:42:54 -0500

    changes condition on which to load dev dependency

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,11 +1,15 @@
+let createLogger = null;
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
+if (process.env.NODE_ENV !== 'production') {
+  createLogger = require('redux-logger');
+}
+// import createLogger from 'redux-logger'
 import reducer from '../reducers'
 import SessionMiddleware from '../middleware/session_middleware'
 
 const middleware = [ thunk, SessionMiddleware ]
-if (process.env.NODE_ENV !== 'production') {
+if (createLogger && process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger())
 }
 

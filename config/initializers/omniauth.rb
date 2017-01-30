9c767b58985ca220b9793d9b06e407e40145310d
Ted Eckel <teckel1029@gmail.com>
2017-01-29 21:51:02 -0500

    changes omniauth config temporarily

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :pocket, ENV['pocket_consumer_key']
 end
 
-Omniauth.config do |config|
-  config.on_failure do
-    redirect_to root_url
-  end
-end
+# Omniauth.config do |config|
+#   config.on_failure do
+#     redirect_to root_url
+#   end
+# end

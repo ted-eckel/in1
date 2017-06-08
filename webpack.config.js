97ef206fffe76ac7e1240fe6b3d15f5056123861
Ted Eckel <teckel1029@gmail.com>
2017-06-08 13:47:53 -0400

    attempt remove minification warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
 var path = require("path");
+var webpack = require("webpack")
+
+var plugins = ['transform-decorators-legacy'];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
@@ -14,7 +35,6 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          plugins: ['transform-decorators-legacy'],
           presets: ['es2015', 'react', 'stage-2']
         }
       },

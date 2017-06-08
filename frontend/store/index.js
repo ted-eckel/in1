dcbad805adf21af4f38d5ade915690c44899edf6
Ted Eckel <teckel1029@gmail.com>
2017-06-08 12:53:34 -0400

    comment out dev code

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -3,21 +3,21 @@ import thunk from 'redux-thunk'
 import reducer from '../reducers'
 
 const middleware = [ thunk ]
-if (process.env.NODE_ENV !== 'production') {
-  const createLogger = require('redux-logger');
-  const logger = createLogger();
-  middleware.push(logger);
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   const createLogger = require('redux-logger');
+//   const logger = createLogger();
+//   middleware.push(logger);
+// }
+//
+// const composeEnhancers =
+//   process.env.NODE_ENV !=='production' &&
+//   typeof window === 'object' &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//       name: "in1box"
+//     }) : compose;
 
-const composeEnhancers =
-  process.env.NODE_ENV !=='production' &&
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      name: "in1box"
-    }) : compose;
-
-// const composeEnhancers = compose;
+const composeEnhancers = compose;
 
 const configureStore = (preloadedState = {}) => createStore(
   reducer, preloadedState, composeEnhancers(
